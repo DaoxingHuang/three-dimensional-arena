b2b4bc47c3d7812d0e30571ebc43e63be9c6ecf5
James <daoxingyue@live.cn>
2024-05-02 17:55:50 +0800

    feat: init project with turbo

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
-import { Card } from "@repo/ui/card";
-import { Code } from "@repo/ui/code";
+import { Card } from "@dxzy/ui/card";
+import { Code } from "@dxzy/ui/code";
 import styles from "./page.module.css";
-import { Button } from "@repo/ui/button";
+import { Button } from "@dxzy/ui/button";
 
 function Gradient({
   conic,
